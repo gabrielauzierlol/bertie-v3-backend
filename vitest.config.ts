10513ad80a8f6c5ada11406b5b45532163f09a39
Gabriel Vasconcelos Auzier <gabriel@loldesign.com.br>
2024-02-23 16:07:42 -0400

    chore: configure core deps

---
+++ vitest.config.ts
@@ -0,0 +1,21 @@
+import swc from 'unplugin-swc'
+import { configDefaults, defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: './',
+    exclude: [...configDefaults.exclude, '**/data/pg/**'],
+  },
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
+  },
+})
